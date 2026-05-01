99e0c1e3d76cdf5c4379f9c56c90924c8e28d5cd
Rohat Orakçı <orakirohat1@gmail.com>
2026-05-02 00:09:38 +0300

    Story 1: Firebase bağlantı ayarları ve internet izinleri eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'login.dart';
@@ -8,9 +9,12 @@ import 'profile.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Firebase'i yeni oluşturduğumuz options dosyasıyla başlatıyoruz
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseDatabase.instance.databaseURL =
+      "https://yazilim-yapimi-eb2a5-default-rtdb.europe-west1.firebasedatabase.app/";
 
   runApp(const MyApp());
 }
@@ -42,16 +46,12 @@ class AuthWrapper extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
+            body: Center(child: CircularProgressIndicator()),
           );
         }
 
         if (snapshot.hasData) {
-          return const MyHomePage(
-            title: 'Kelime Ezberleme Ana Sayfa',
-          );
+          return const MyHomePage(title: 'Kelime Ezberleme Ana Sayfa');
         } else {
           return const LoginPage();
         }
@@ -61,10 +61,7 @@ class AuthWrapper extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-    required this.title,
-  });
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -81,7 +78,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
 
   void _changePage(int index) {
     setState(() {
@@ -109,27 +105,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
       case 1:
         return const Center(
-          child: Text(
-            'Kelimeler Sayfası',
-            style: TextStyle(fontSize: 24),
-          ),
+          child: Text('Kelimeler Sayfası', style: TextStyle(fontSize: 24)),
         );
 
       case 2:
         return const Center(
-          child: Text(
-            'İstatistik Sayfası',
-            style: TextStyle(fontSize: 24),
-          ),
+          child: Text('İstatistik Sayfası', style: TextStyle(fontSize: 24)),
         );
 
       case 3:
-      return const ProfilePage();
+        return const ProfilePage();
 
       default:
-        return const Center(
-          child: Text('Sayfa bulunamadı'),
-        );
+        return const Center(child: Text('Sayfa bulunamadı'));
     }
   }
 
@@ -137,13 +125,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-    backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-  title: Text(widget.title),
-),
-
-      body: Center(
-        child: _buildBody(),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
+
+      body: Center(child: _buildBody()),
 
       floatingActionButton: _selectedIndex == 0
           ? FloatingActionButton(
@@ -160,10 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedItemColor: Colors.deepPurple,
         unselectedItemColor: Colors.grey,
         items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Ana Sayfa',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Ana Sayfa'),
           BottomNavigationBarItem(
             icon: Icon(Icons.menu_book),
             label: 'Kelimeler',
@@ -172,10 +155,7 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.bar_chart),
             label: 'İstatistik',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profil',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
         ],
       ),
     );
