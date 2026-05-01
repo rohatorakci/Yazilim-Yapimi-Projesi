8c080374fdbb6ef72e8f533f472b25a23d545ba3
Muhammet Orucoglu <muhammetorucoglugfb@gmail.com>
2026-05-02 00:17:38 +0300

    üstte duran ana sayfa bölümünü kaldırdım

--- lib/main.dart
+++ lib/main.dart
@@ -136,10 +136,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-    backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-  title: Text(widget.title),
-),
+
 
       body: Center(
         child: _buildBody(),
