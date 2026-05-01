69c8a3bc9cfaa28308ef6f89e141acf734a96c8e
Muhammet Orucoglu <muhammetorucoglugfb@gmail.com>
2026-05-02 00:33:32 +0300

    menü eklendi, menüdeki profile sayfası yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -124,17 +124,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-
-
-      body: Center(
-        child: _buildBody(),
-=======
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
->>>>>>> ef0b1cc589750964c039f96e80c5bc802bef277a
-      ),
 
       body: Center(child: _buildBody()),
 
